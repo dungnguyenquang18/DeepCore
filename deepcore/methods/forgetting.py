ebe04ab4138d8414f057110ef68917d7a32950c0
dungnguyenquang18 <dugnam18@gmail.com>
2024-11-29 19:13:45 +0700

    sua lai indx

--- deepcore/methods/forgetting.py
+++ deepcore/methods/forgetting.py
@@ -75,7 +75,7 @@ class Forgetting(EarlyTrain):
             top_examples = np.array([], dtype=np.int64)
             for c in range(self.num_classes):
                 # c_indx = self.train_indx[self.dst_train.targets == c]
-                c_indx = self.train_idx[torch.argmax(self.dst_train.y) == c]
+                c_indx = self.train_indx[torch.argmax(self.dst_train.y) == c]
                 budget = round(self.fraction * len(c_indx))
                 top_examples = np.append(top_examples,
                                     c_indx[np.argsort(self.forgetting_events[c_indx].cpu().numpy())[::-1][:budget]])
