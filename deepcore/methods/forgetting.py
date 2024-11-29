7eb68070dc3926f3421dab7c494ec62bc63383ec
dungnguyenquang18 <dugnam18@gmail.com>
2024-11-29 18:42:06 +0700

    sua loi device

--- deepcore/methods/forgetting.py
+++ deepcore/methods/forgetting.py
@@ -33,12 +33,10 @@ class Forgetting(EarlyTrain):
             _, predicted = torch.max(outputs.data, 1)
 
             cur_acc = (predicted == targets).clone().detach().requires_grad_(False).type(torch.float32)
-            index_tensor = torch.tensor(batch_inds)[(self.last_acc[batch_inds] - cur_acc) > 0.01].to(self.forgetting_events.device)  
+            index_tensor = torch.tensor(batch_inds, device=self.forgetting_events.device)[(self.last_acc[batch_inds] - cur_acc) > 0.01]
 
             self.forgetting_events[index_tensor] += 1.
             self.last_acc[batch_inds] = cur_acc
-            # self.forgetting_events[torch.tensor(batch_inds)[(self.last_acc[batch_inds]-cur_acc)>0.01]]+=1.
-            # self.last_acc[batch_inds] = cur_acc
 
     def while_update(self, outputs, loss, targets, epoch, batch_idx, batch_size):
         self.train_loss += loss.item()
